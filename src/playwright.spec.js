15ef20e40feb7c79fcadfa95b9ed39d4088567e7
Arvin Sevilla <asevilla@yondu.com>
2020-03-23 01:22:07 +0800

    Clean up test structure and add vuetify dialog/modal test

--- src/playwright.spec.js
+++ src/playwright.spec.js
@@ -8,74 +8,54 @@ const SOME_TEXT = "asdfasdf@jkljkl.com"
 let browser
 let page
 describe.each([
-    ["chromium", true],
-    ["firefox", true],
-    ["webkit", true],
-    ["chromium", false],
-    ["firefox", false],
-    ["webkit", false],
-])("type test", (browserType, headless) => {
-    beforeAll(async () => {
-        browser = await playwright[browserType].launch({ headless })
+    ["chromium"],
+    ["firefox"],
+    ["webkit"],
+])("%s type and fill test", (browserType) => {
+    describe.each([
+        [false],
+        [true],
+    ])("headless: %s", (headless) => {
+        beforeAll(async () => {
+            browser = await playwright[browserType].launch({ headless })
 
-        const context = await browser.newContext()
-        page = await context.newPage()
-    })
-
-    afterAll(async () => {
-        await browser.close()
-    })
-
-    it.each([
-        ["type"],
-        ["fill"],
-    ])(`${browserType}${headless ? "(headless)" : "(non-headless)"}: should %s out all inputs inside modal`, async (method) => {
-        // please launch in shell first:
-        // `python3 -m http.server -d src`
-        await page.goto("http://localhost:8000")
-
-        await page.click("button")
-
-        const modal = await page.waitForSelector("#myModal .modal-content", {
-            waitFor: "visible"
+            const context = await browser.newContext()
+            page = await context.newPage()
         })
 
-        await page[method]("input[type=number]", SOME_NUMBER)
-        const texts = await page.$$("input[type=text]")
-        for (let i = 0; i < texts.length; ++i) {
-            await texts[i][method](SOME_TEXT)
+        afterAll(async () => {
+            await browser.close()
+        })
+
+        describe.each([
+            ["type"],
+            ["fill"],
+        ])("method: %s", (method) => {
+            it.each([
+                ["inside bootstrap modal", "index.html"],
+                ["outside modal", "inputs.html"],
+                ["inside vuetify modal", "vuetify.html"],
+            ])(`should update input values %s`, playwrightTest(browserType, headless, method), 35000)
+        })
+    })
+})
+
+function playwrightTest(browserType, headless, method) {
+    return async (_, htmlFile) => {
+        // please launch in shell first:
+        // `python3 -m http.server -d src`
+        await page.goto("http://localhost:8000/" + htmlFile)
+
+        let modal = null
+        if (await page.$("button")) {
+            await page.click("button")
+            modal = await page.waitForSelector(".modal-content", {
+                waitFor: "visible"
+            })
         }
-        await page[method]("input[type=email]", SOME_TEXT)
 
-        await modal.screenshot({ path: `ss/${browserType + headless}3modal.png` })
-        await modal.screenshot({ fullPage: true, path: `ss/${browserType + headless}4modalfull.png` })
-
-        const numVal = await page.$eval("input[type=number]", (input) => input.value)
-        expect(numVal).toBe(SOME_NUMBER)
-
-        const textVals = await page.$$eval("input[type=text]", (inputs) => inputs.map((input) => input.value))
-        textVals.forEach((textVal) => {
-            expect(textVal).toBe(SOME_TEXT)
-        })
-
-        const emailVal = await page.$eval("input[type=email]", (input) => input.value)
-        expect(emailVal).toBe(SOME_TEXT)
-
-        expect(await page.$("text=I AM TOP OF PAGE")).not.toBeNull()
-        expect(await page.$("text=I AM BOTTOM OF PAGE")).not.toBeNull()
-    }, 35000)
-
-    it.each([
-        ["type"],
-        ["fill"],
-    ])(`${browserType}${headless ? "(headless)" : "(non-headless)"}: should %s out all inputs`, async (method) => {
-        // please launch in shell first:
-        // `python3 -m http.server -d src`
-        await page.goto("http://localhost:8000/inputs.html")
-
-        // await page.click("button")
-
-        await page.screenshot({ fullPage: true, path: `ss/${browserType + headless}1before.png` })
+        const uniqueId = [browserType, headless ? "headless" : "non-headless", method, htmlFile].join("-")
+        await page.screenshot({ fullPage: true, path: `ss/${uniqueId}-1before.png` })
 
         const numbers = await page.$$("input[type=number]")
         for (let i = 0; i < numbers.length; ++i) {
@@ -90,7 +70,11 @@ describe.each([
             await emails[i][method](SOME_TEXT)
         }
 
-        await page.screenshot({ fullPage: true, path: `ss/${browserType + headless}2after.png` })
+        await page.screenshot({ fullPage: true, path: `ss/${uniqueId}-2after.png` })
+
+        if (modal) {
+            await modal.screenshot({ path: `ss/${uniqueId}-3modal.png` })
+        }
 
         const numVals = await page.$$eval("input[type=number]", (inputs) => inputs.map((input) => input.value))
         numVals.forEach((numVal) => {
@@ -106,6 +90,5 @@ describe.each([
         emailVals.forEach((emailVal) => {
             expect(emailVal).toBe(SOME_TEXT)
         })
-    }, 35000)
-})
-
+    }
+}
