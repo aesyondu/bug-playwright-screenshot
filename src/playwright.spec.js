2de9dbf62c9ae00030019db881a2bcec290c9653
Arvin Sevilla <asevilla@yondu.com>
2020-03-22 23:57:34 +0800

    Add test for page.type and for input elements outside modal

--- src/playwright.spec.js
+++ src/playwright.spec.js
@@ -26,7 +26,10 @@ describe.each([
         await browser.close()
     })
 
-    it(`${browserType}${headless ? "(headless)" : "(non-headless)"}: should type out all inputs`, async () => {
+    it.each([
+        ["type"],
+        ["fill"],
+    ])(`${browserType}${headless ? "(headless)" : "(non-headless)"}: should %s out all inputs inside modal`, async (method) => {
         // please launch in shell first:
         // `python3 -m http.server -d src`
         await page.goto("http://localhost:8000")
@@ -37,12 +40,12 @@ describe.each([
             waitFor: "visible"
         })
 
-        await page.fill("input[type=number]", SOME_NUMBER)
+        await page[method]("input[type=number]", SOME_NUMBER)
         const texts = await page.$$("input[type=text]")
         for (let i = 0; i < texts.length; ++i) {
-            await texts[i].fill(SOME_TEXT)
+            await texts[i][method](SOME_TEXT)
         }
-        await page.fill("input[type=email]", SOME_TEXT)
+        await page[method]("input[type=email]", SOME_TEXT)
 
         await modal.screenshot({ path: `ss/${browserType + headless}3modal.png` })
         await modal.screenshot({ fullPage: true, path: `ss/${browserType + headless}4modalfull.png` })
@@ -60,6 +63,49 @@ describe.each([
 
         expect(await page.$("text=I AM TOP OF PAGE")).not.toBeNull()
         expect(await page.$("text=I AM BOTTOM OF PAGE")).not.toBeNull()
+    }, 35000)
+
+    it.each([
+        ["type"],
+        ["fill"],
+    ])(`${browserType}${headless ? "(headless)" : "(non-headless)"}: should %s out all inputs`, async (method) => {
+        // please launch in shell first:
+        // `python3 -m http.server -d src`
+        await page.goto("http://localhost:8000/inputs.html")
+
+        // await page.click("button")
+
+        await page.screenshot({ fullPage: true, path: `ss/${browserType + headless}1before.png` })
+
+        const numbers = await page.$$("input[type=number]")
+        for (let i = 0; i < numbers.length; ++i) {
+            await numbers[i][method](SOME_NUMBER)
+        }
+        const texts = await page.$$("input[type=text]")
+        for (let i = 0; i < texts.length; ++i) {
+            await texts[i][method](SOME_TEXT)
+        }
+        const emails = await page.$$("input[type=email]")
+        for (let i = 0; i < emails.length; ++i) {
+            await emails[i][method](SOME_TEXT)
+        }
+
+        await page.screenshot({ fullPage: true, path: `ss/${browserType + headless}2after.png` })
+
+        const numVals = await page.$$eval("input[type=number]", (inputs) => inputs.map((input) => input.value))
+        numVals.forEach((numVal) => {
+            expect(numVal).toBe(SOME_NUMBER)
+        })
+
+        const textVals = await page.$$eval("input[type=text]", (inputs) => inputs.map((input) => input.value))
+        textVals.forEach((textVal) => {
+            expect(textVal).toBe(SOME_TEXT)
+        })
+
+        const emailVals = await page.$$eval("input[type=email]", (inputs) => inputs.map((input) => input.value))
+        emailVals.forEach((emailVal) => {
+            expect(emailVal).toBe(SOME_TEXT)
+        })
     }, 35000)
 })
 
